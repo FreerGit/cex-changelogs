659498ed7d8b51b8b3160c1a52bd4b23996f8290
FreerGit <fredrik-98@live.se>
2024-01-05 00:22:51 +0100

    getting wrong handle eod

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,20 @@
 const std = @import("std");
 const assert = std.debug.assert;
 const hmap = std.hash_map;
+const print = std.debug.print;
 
-fn setup_changelog_files(comptime names: []const []const u8) ![]std.fs.File {
+fn setup_changelog_files(comptime names: []const []const u8, allocator: std.mem.Allocator) ![]std.fs.File {
     const cwd = std.fs.cwd();
     var files: [names.len]std.fs.File = undefined;
-    // var idx = 0;
-    inline for (names[0..], 0..) |name, idx| {
-        const open_file = cwd.openFile("./changelogs/" ++ name ++ ".txt", .{ .mode = .read_write }) catch blk: {
-            break :blk try cwd.createFile("./changelogs/" ++ name ++ ".txt", std.fs.File.CreateFlags{ .read = true });
+
+    for (names, 0..) |name, idx| {
+        const path = try std.fmt.allocPrint(allocator, "{s}{s}{s}", .{ "./changelogs/", name, ".txt" });
+        std.debug.print("{d} {s} {s}\n", .{ idx, name, path });
+        const open_file = cwd.openFile(path, .{ .mode = .read_write }) catch blk: {
+            break :blk try cwd.createFile(path, std.fs.File.CreateFlags{ .read = true });
         };
         files[idx] = open_file;
-        std.debug.print("{d} {any}", .{ idx, files[idx] });
+        std.debug.print("{any}\n", .{files[idx]});
     }
 
     return files[0..];
@@ -27,16 +30,19 @@ fn setup_changelog_files(comptime names: []const []const u8) ![]std.fs.File {
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
     defer arena.deinit();
-    const names = [_][]const u8{ "bybit", "binance" };
-    const files = try setup_changelog_files(names[0..]);
+    const names = [_][]const u8{ "bybit", "binance", "OKX" };
+    const files = try setup_changelog_files(names[0..], arena.allocator());
     var map = std.StringHashMap([]const u8).init(arena.allocator());
     for (files, names) |file, name| {
         var read_buffer: [1024 * 1024 * 16]u8 = undefined;
+        std.debug.print("{d} {any}", .{ 0, file });
+        std.debug.print("hej\n", .{});
         const bytes_read = try file.readAll(&read_buffer);
+        print("{d}\n", .{bytes_read});
         try map.put(name, read_buffer[0..bytes_read]);
+        std.debug.print("file: {any}\n", .{map.get(name)});
         // defer file.close();
     }
-    std.debug.print("file: {s}\n", .{map.get("binance").?});
     // const file = try std.fs.cwd().createFile("./changelogs/binance.txt", std.fs.File.CreateFlags{ .read = true });
     // defer file.close();
     // // os.read("./changelogs/binance.txt", &binance_buffer);
