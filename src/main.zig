2c22246cf585380b36750b7fe2a06ab2487a7b9c
FreerGit <fredrik-98@live.se>
2024-01-07 01:41:23 +0100

    feat: finds diffs - done

--- src/main.zig
+++ src/main.zig
@@ -15,6 +15,10 @@ fn get_api_docs(ex_info: ExchangeInfo, allocator: std.mem.Allocator) ![]const u8
     return try allocator.dupe(u8, html);
 }
 
+fn get_filename_path(name: []const u8, allocator: std.mem.Allocator) ![]const u8 {
+    return try std.fmt.allocPrint(allocator, "{s}{s}{s}", .{ "./changelogs/", name, ".txt" });
+}
+
 fn setup_changelog_files(
     ex_info: []const ExchangeInfo,
     files: []std.fs.File,
@@ -22,7 +26,7 @@ fn setup_changelog_files(
 ) !void {
     const cwd = std.fs.cwd();
     for (ex_info, 0..) |e, idx| {
-        const path = try std.fmt.allocPrint(allocator, "{s}{s}{s}", .{ "./changelogs/", e.name, ".txt" });
+        const path = try get_filename_path(e.name, allocator);
         std.debug.print("{d} {s} {s}\n", .{ idx, e.name, path });
         const open_file = cwd.openFile(path, .{ .mode = .read_write }) catch blk: {
             const file = try cwd.createFile(path, std.fs.File.CreateFlags{ .read = true });
@@ -33,13 +37,6 @@ fn setup_changelog_files(
         files[idx] = open_file;
     }
 }
-
-//  read file
-//  if not exists
-//      create
-//  loop http read
-//  write to file any diffs
-//  delete changelog
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
@@ -53,35 +50,29 @@ pub fn main() !void {
     } };
     var files: [exchange_info.len]std.fs.File = undefined;
     try setup_changelog_files(exchange_info[0..], &files, arena.allocator());
-    var map = std.StringHashMap([]const u8).init(arena.allocator());
-    for (files, exchange_info) |file, exc_info| {
-        const read_buffer = try file.readToEndAlloc(arena.allocator(), 1024 * 1024 * 16);
-        try map.put(exc_info.name, read_buffer);
-    }
+    const cwd = std.fs.cwd();
 
     var idx: usize = 0;
     while (true) {
-        const last_changelog = map.get(exchange_info[idx].name).?;
+        const diff_file = try cwd.createFile("diff.txt", std.fs.File.CreateFlags{ .read = true });
         const current_changelog = try get_api_docs(exchange_info[idx], arena.allocator());
-        print("{any}", .{std.mem.eql(u8, last_changelog, current_changelog)});
+        try diff_file.writeAll(current_changelog);
 
+        const path = try get_filename_path(exchange_info[idx].name, arena.allocator());
+        const argv = .{ "diff", path, "diff.txt" };
+        var proc = std.ChildProcess.init(&argv, arena.allocator());
+        try proc.spawn();
+        const term = try proc.wait();
+        diff_file.close();
+        if (term.Exited != 0) {
+            print(
+                "The above diffs from the following exchange: {s}\n",
+                .{exchange_info[idx].name},
+            );
+            break;
+        }
         idx += 1;
         if (idx == exchange_info.len) idx = 0;
         std.time.sleep(std.time.ns_per_s * 1);
     }
-
-    // const file = try std.fs.cwd().createFile("./changelogs/binance.txt", std.fs.File.CreateFlags{ .read = true });
-    // defer file.close();
-    // // os.read("./changelogs/binance.txt", &binance_buffer);
-
-    // std.debug.print("{d}, {s}\n{s}\n", .{ bytes_read, binance_buffer[0..100], binance_html[0..100] });
-    // assert(std.mem.eql(u8, binance_buffer[0..bytes_read], binance_html));
-    // std.debug.print("{s}", .{binance_html});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
